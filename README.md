a5b299877d231def14905f6b3f6bf90bf38ab616
reisvaynard <reisvaynard@gmail.com>
2021-11-03 18:54:51 +0700

    Session 3 and README

---
+++ README.md
@@ -0,0 +1,4 @@
+Nama : R. Ibrahim Senoaji
+Kode Peserta : PYTN067ONL002
+
+Project : Introduction to Python for Data Science
---
+++ mod.py
@@ -0,0 +1,13 @@
+s = "Hacktiv8-PTP Python for DS"
+a = [100, 200, 300]
+x = ['saya', 'anda']
+
+def foo(arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass
+
+print(s)
+print(a)
+print(x)
